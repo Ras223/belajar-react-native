66a3cd902571242a8e269f6aaabbae39fbac9c53
Rizky Aditya Syahputra <93401116+Ras223@users.noreply.github.com>
2023-11-16 09:25:38 +0700

    export import

--- App.tsx
+++ App.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
-import {Image, Text, TextInput, View} from 'react-native';
+import {ScrollView, View} from 'react-native';
+import SampleComponent from './SampleComponent';
+import StylingComponent from './StylingComponent';
 
 const App = () => {
   return (
-    <View>
-      <View style={{width: 80, height: 80, backgroundColor: 'red'}} />
-      <Text>prawito</Text>
-      <Hudoro />
-      <Text>mila</Text>
-      <Text>azamy</Text>
-      <Photo />
-      <TextInput style={{borderWidth: 1}} />
-    </View>
-  );
-};
-
-const Hudoro = () => {
-  return <Text>Hudoro mangku negoro</Text>;
-};
-
-const Photo = () => {
-  return (
-    <Image
-      source={{uri: 'http://via.placeholder.com/640x360'}}
-      style={{width: 100, height: 100}}
-    />
+    <ScrollView>
+      <View>
+        <SampleComponent />
+        <StylingComponent />
+      </View>
+    </ScrollView>
   );
 };
 
